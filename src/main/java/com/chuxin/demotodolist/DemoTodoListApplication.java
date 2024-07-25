88a07f3ed7f20827447bb6125d1bc9ad37cdfd35
chuxin <15115359055@139.com>
2024-07-25 20:56:36 +0800

    feat: 项目初始化

---
+++ src/main/java/com/chuxin/demotodolist/DemoTodoListApplication.java
@@ -0,0 +1,14 @@
+package com.chuxin.demotodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTodoListApplication {
+
+    public static void main(String[] args) {
+        System.out.println("chuXin=======>");
+        SpringApplication.run(DemoTodoListApplication.class, args);
+    }
+
+}
