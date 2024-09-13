82b54e3e7df15841140f232d423c1c2e63e967b6
chuxin <15115359055@139.com>
2024-09-13 15:11:39 +0800

    feat: 实现 AuthController 业务逻辑代码

--- src/main/java/com/chuxin/demotodolist/module/auth/controller/AuthController.java
+++ src/main/java/com/chuxin/demotodolist/module/auth/controller/AuthController.java
@@ -1,4 +1,27 @@
 package com.chuxin.demotodolist.module.auth.controller;
 
+import groovy.util.logging.Slf4j;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "01.认证中心")
+@RestController
+@RequestMapping("/api/v1/auth")
+@Slf4j
 public class AuthController {
+
+    @PostMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @Operation(summary = "获取验证码")
+    @GetMapping("/captcha")
+    public String getCaptcha(){
+        return "captcha";
+    }
 }
