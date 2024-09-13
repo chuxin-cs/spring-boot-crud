567dc2aa7ad8dde1e84b4ca4935205d7f459c5c4
chuxin <15115359055@139.com>
2024-09-13 09:45:34 +0800

    feat: 新增 system/service 模块

---
+++ src/main/java/com/chuxin/demotodolist/system/service/UserService.java
@@ -0,0 +1,4 @@
+package com.chuxin.demotodolist.system.service;
+
+public interface UserService {
+}
---
+++ src/main/java/com/chuxin/demotodolist/system/service/impl/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.chuxin.demotodolist.system.service.impl;
+
+public class UserServiceImpl {
+}
